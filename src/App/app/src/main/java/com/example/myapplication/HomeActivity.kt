e75e3afb477ca6a442137b0b5105f86507afec44
Leon Lowa <s83824@htw-dresden.de>
2022-12-29 12:35:25 +0100

    erneuter binding versuch

---
+++ src/App/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            // Handle item selection here
+            true
+        }
+    }
+}
