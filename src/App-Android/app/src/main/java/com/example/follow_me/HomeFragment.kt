a27f75aad41bb5fcb786eb31a4e17c2c5c186692
Jonas Pfeiffer <s83836@informatik.htw-dresden.de>
2023-01-01 20:27:34 +0100

    Login Button und Roboter Liste implementiert

--- src/App-Android/app/src/main/java/com/example/follow_me/HomeFragment.kt
+++ src/App-Android/app/src/main/java/com/example/follow_me/HomeFragment.kt
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_robot.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +31,7 @@ class HomeFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -56,4 +62,9 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+
+
+
+
 }
---
+++ src/App-Android/app/src/main/java/com/example/follow_me/Robot.kt
@@ -0,0 +1,5 @@
+package com.example.follow_me
+
+data class Robot(
+    val name: String
+)
